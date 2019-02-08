3e967444594dd81dfaf26d5cdf8a0053641d44b8
Lilian Deloche <lilian.deloche@puck.fr>
2019-02-08 14:20:35 +0100

    use sys.write for update

--- assets/check_relocating.py
+++ assets/check_relocating.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+import sys
 import requests
 http_response=requests.get('http://127.0.0.1:9200/_cat/shards')
 lshard=http_response.text
 if lshard.find('RELOCATING') == -1:
-    print ('NOT RELOCATING')
+    sys.stdout.write('NOT RELOCATING')
 else:
-    print ('RELOCATING')
+    sys.stdout.write('RELOCATING')
--- assets/cluster_color.py
+++ assets/cluster_color.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import requests
 import json
+import sys
 
 http_response = requests.get('http://127.0.0.1:9200/_cluster/health')
 jsonresponse = json.loads(http_response.text)
-print (jsonresponse['status'])
+sys.stdout.write(jsonresponse['status'])
